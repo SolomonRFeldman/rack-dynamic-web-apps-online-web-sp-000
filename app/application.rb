cfb48bd2fe25d6939bfafe09515682c9356635bd
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-19 00:18:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,8 @@ class Application
     num_3 = Kernel.rand(1..20)
     
     resp.write "#{num_1}"
+    resp.write "#{num_2}"
+    resp.write "#{num_3}"
     
     if num_1 == num_2 && num_2 == num_3
       resp.write "You Win"
